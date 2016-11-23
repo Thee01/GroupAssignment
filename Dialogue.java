aa81ffe4b6d46a4ffc364ae738ad881b2c2db5e4
Thee01 <kennethpduran@hotmail.com>
2016-11-22 22:53:38 -0700

    Add files via upload

---
+++ Dialogue.java
@@ -0,0 +1,11 @@
+/****************************
+* Author: Chloe and Jude
+****************************/
+package finalProject1400;
+
+public class Dialogue
+{
+   public String greeting = "Wassup dawg wanna know ur sign";
+   public String conclusion = "yo yo i'm always right";
+
+}
