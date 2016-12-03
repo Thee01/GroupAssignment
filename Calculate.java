5b07565ffa213b5e8998cc059ec7af5e4c5c797b
ChloeBleak <chloebleak@gmail.com>
2016-12-03 00:21:48 -0700

    zodiacSigns[] because answers class is gone
    we were originally calling the zodiac list in the answers class to get the sign but since we're not using that anymore i put it in here. 
    I also changed calculateUsersZodiacSign() to a public method that returns an int. please review the method getUsersZodiacSign() for duplicate code because of it.

--- Calculate.java
+++ Calculate.java
@@ -2,39 +2,67 @@
 * Author: Chloe
 * add all values from user choices and 
 * calculate sign
-* reading from array starts at 1~~~~~~~
 *********************************************/
-package finalProject1400;
+package zodiacGame;
 
 import java.util.Scanner;
-
-public class Calculate {
-	Scanner input = new Scanner(System.in);
-	private int[] zodiacChoices = new int[13];
-	private int zodiacSignGuess;
-
-	public int getInput(int userChoice) {
+/**
+ * @author chloe_000
+ *
+ */
+public class Calculate{
+   Scanner input = new Scanner(System.in);
+   private int[] zodiacChoices = new int[13];
+   private int zodiacSignGuess;
+   private int userChoice;
+   
+   public int getInput(){
+      this.userChoice = userChoice;	
 		do {
 			userChoice = input.nextInt();
-		} while (userChoice >= 1 || userChoice <= 12);
-
-		return userChoice;
+		}while(userChoice >= 1 || userChoice <= 12);       
+      return userChoice;
+   }
+   
+  	public void addUserChoice(int index){
+      zodiacChoices[index]++; 
+	   return; 
 	}
+	
+  public int calculateUsersZodiacSign(){
+        for(int i = 1; i <= 12; i++)
+        {
+            if(zodiacChoices[i] > zodiacSignGuess)
+            {
+                zodiacSignGuess = i;
+            }
+        }
+        return zodiacSignGuess;
+   }
+  
+   public String zodiacSign(int n){
+	   String[] signs = {
+			   "n/a",
+			   "Aries",
+			   "Taurus",
+			   "Gemini",
+			   "Cancer",
+			   "Leo",
+			   "Virgo",
+			   "Libra",
+			   "Scorpio",
+			   "Sagittarius",
+			   "Capricorn",
+			   "Aquarius",
+			   "Pisces"};
+	   return signs[n];
+   }
+   
+   public int getUsersZodiacSign(){ //duplicate code??
+      calculateUsersZodiacSign();
+      return zodiacSignGuess;
+   }
 
-	public void addUserChoice(int index) {
-		zodiacChoices[index]++;
-	}
-
-	private void calculateUsersZodiacSign() {
-		for (int i = 0; i < 12; i++) {
-			if (zodiacChoices[i] > zodiacSignGuess) {
-				zodiacSignGuess = i;
-			}
-		}
-	}
-
-	public int getUsersZodiacSign() {
-		calculateUsersZodiacSign();
-		return zodiacSignGuess;
-	}
 }
+
+
