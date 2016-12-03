6ecca444a2c0931588d643093666964bea751263
ChloeBleak <chloebleak@gmail.com>
2016-12-02 19:09:46 -0700

    complete ascii art

--- Answers.java
+++ Answers.java
@@ -1,31 +1,89 @@
-/****************************
-* 
-****************************/
-package finalProject1400;
+/*******************************************
+* Author: Jude and Chloe
+*
+*******************************************/
 
-public class ZodiacAscii {
-	//oh heyyyyy
-	/**
-	 * 
-	 * @param index
-	 * @return
-	 */
-	public String getAnswer(int index) {
-		return answers[index];
-	}
-	    
-	public String[] answers = {
-	     "n/a",
-	     "Aries",
-	     "Taurus",
-	     "Gemini",
-	     "Cancer",
-	     "Leo",
-	     "Virgo",
-	     "Libra",
-	     "Scorpio",
-	     "Sagittarius",
-	     "Capricorn",
-	     "Aquarius",
-	     "Pisces"};	    
+public class ZodiacASCIIArt{
+   public static void main(String[] args){
+      // aries
+      System.out.printf(" .-.   .-.  %n" +
+                        "(_  \\ /  _)%n" +       
+                        "     |      %n" +
+                        "     |      %n" );
+         
+     // taurus 
+     System.out.printf(" .     .    %n" +
+                       " '.___.'    %n" +       
+                       " .'   `.    %n" +
+                       ":       :   %n" +
+                       ":       :   %n" +
+                       " `.___.'    %n");
+
+      // gemini
+      System.out.printf("._____.   %n" +
+                        "  | |     %n" +
+                        "  | |     %n" +
+                        " _|_|_    %n" +
+                        "'     '   %n");
+
+      // cancer
+      System.out.printf("   .--.    %n" +
+                        "  /   _`.  %n" +
+                        " (_) ( )   %n" +
+                        "'.    /    %n" +
+                        "  `--'     %n");   
+                              
+      
+      // leo
+      System.out.printf("    .--.  %n" +
+                        "   (    ) %n" +       
+                        "  (_)  /  %n" +
+                        "      (_, %n");
+                        
+      // virgo
+      System.out.printf(" _          %n" +
+                        "' `:--.--.   %n" + 
+                        "   |  |  |_  %n" +
+                        "   |  |  | ) %n" +
+                        "   |  |  |/  %n" +
+                        "        (J   %n"); 
+      
+      // libra
+      System.out.printf("     __      %n" +
+                        "___.'  '.___ %n" +
+                        "____________ %n");
+      
+      // scorpio
+      System.out.printf(" _              %n" +
+                        "' `:--.--.      %n" + 
+                        "   |  |  |      %n" +
+                        "   |  |  |      %n" +
+                        "   |  |  |  .., %n" +
+                        "         `---': %n");
+                        
+     // sagittarius
+     System.out.printf("      ... %n" +
+                       "      .': %n" +
+                       "    .'    %n" +
+                       "`..'      %n" +
+                       ".'`.      %n");
+                       
+     // capricorn
+     System.out.printf("        _   %n" +
+                       "\\      /_) %n" +
+                       " \\    /`.  %n" +
+                       "  \\  /   ; %n" +
+                       "   \\/ __.  %n");
+    // aquarius
+    System.out.printf(".-'-._.-'-._.- %n" +
+                      ".-'-._.-'-._.- %n");
+    
+    // pisces
+    System.out.printf("`-.    .-' %n" +
+                      "   :  :    %n" +
+                      " --:--:--  %n" + 
+                      "   :  :    %n" +
+                      ".-'    `-. %n");
+
+   }
 }
