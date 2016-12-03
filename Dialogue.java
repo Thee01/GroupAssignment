0df4a0b20cbc6ebd4155398a70bc83be651c60cd
ChloeBleak <chloebleak@gmail.com>
2016-12-02 19:32:01 -0700

    updated with temp greeting dialog

--- Dialogue.java
+++ Dialogue.java
@@ -1,11 +1,39 @@
 /****************************
 * Author: Chloe and Jude
 ****************************/
-package finalProject1400;
+package zodiacGame;
 
-public class Dialogue
-{
-   public String greeting = "Wassup dawg wanna know ur sign";
-   public String conclusion = "yo yo i'm always right";
+import java.util.Scanner;
+public class Dialogue{
+	
+	Scanner input = new Scanner(System.in);
+   
+   public void greeting(){
+	   System.out.println("Hi. What's your name?");
+	   String name = input.nextLine();
+	   System.out.printf("Hi %s. My name is 'Zodiac guess game 2.0 %n"
+	   		+ "Do you want to talk about astrology? (Yes/No)", name);
+	   String yesOrNo = input.next();
+	   switch(yesOrNo.toUpperCase()){
+	   		case "YES":
+	   			System.out.println("Yas! I bet I can guess what your zodiac sign is.");
+	   			break;
+	   		case "NO":
+	   		System.out.println("What, like you have something better to do? SIT DOWN. I bet I can guess what your sign is.");
+	   			break;
+	   			
+	    return;
+	}
+	   
+	public void question1(){
+		System.out.println("Good for you! Next Question.");	
+	}
+		   
+	   
+	}
+	   
+	   
+	   
+   }
 
 }
