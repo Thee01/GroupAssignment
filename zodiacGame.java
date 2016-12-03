a4eb210965051a231446c6e60d51d69cd73cf925
ChloeBleak <chloebleak@gmail.com>
2016-12-02 22:37:22 -0700

    removed methods, added reply String[] to for loop

--- zodiacGame.java
+++ zodiacGame.java
@@ -6,36 +6,18 @@ public class ZodiacGame {
 	private static Scanner input = new Scanner(System.in);
 	private static Calculate calculate = new Calculate();
 	private static Questions questions = new Questions();
+	private static Dialogue dialogue = new Dialogue();
 
 	public static void main(String[] args) {
-		/*for (int i = 0; i < 3; i++) {
+
+		for (int i = 0; i < 3; i++) { //can we please have this start at 1?
 			displayQuestions(i);
 			getInput();
+			dialogue.questionReplies(i);
 		}
-		displayUsersZodiacSign();*/
-		int userChoice = 0;
-		
-	Dialogue dialogue = new Dialogue();
+		displayUsersZodiacSign();
 	
-	dialogue.greeting();
-	displayQuestions(1);
-	getInput();
-	dialogue.question1();
-	
-	displayQuestions(2);
-	getInput();
-	dialogue.question2();
-	
-	displayQuestions(3);
-	getInput();
-	if(userChoice < 13 || userChoice > 0){
-		dialogue.question3();
-	} else if(userChoice == 13)
-		dialogue.question3Choice13();
 	}
-	
-	
-
 	/**
 	 * displays questions
 	 */
