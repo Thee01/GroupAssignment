55446d5affcb4e22f79cd27c904b15c3315d5ac9
Thee01 <kennethpduran@hotmail.com>
2016-12-05 17:39:16 -0700

    added dialogue that shows after user enters optii

--- Dialogue.java
+++ Dialogue.java
@@ -38,40 +38,29 @@ public class Dialogue {
 						System.out.printf("%nWanna try that again?%n%n"); //get this to loop
 			}
 		} while(yesOrNoChoosen == false);
-	}	
-		/*
-		//do{
-			System.out.printf(
-					"Hi %s. My name is 'Zodiac guess game 2.0. Do you want to talk about astrology? (Yes/No) ", name);
-			String yesOrNo = input.next();
-		
-			switch (yesOrNo.toUpperCase()) {
-				case "YES":
-					System.out.println("Yas! I bet I can guess what your zodiac sign is. Let's do this.");
-					break;
-				case "NO":
-					System.out.println(
-							"What, like you have something better to do? SIT DOWN. I bet I can guess what your sign is. Let's do this.");
-					break;
-				default:
-						System.out.println("Wanna try that again?"); //get this to loop
-			}
-		//} while(switch == default);
-		*/
-//Kenneth dec 3
+	    }	
 	}
-
-	public String questionReplies(int n){
-		String[] questionRepliesArray = {
-			"n/a",
-			"Good for you! Next Question.", // Q1
-			"Yikes. Okay, next.", // Q2
-			"Honestly I wouldn't want to do that but to each their own.", // Q3
-			//continue with addt'l question replies
-			};
+//dec 5 kenneth duran added the rest of the dialogue that displays after the user answers a question
+	public String questionReplies(int n) {
+		String[] questionRepliesArray = { 		"n/a",
+				"Good for you! Next Question.", // Q1
+				"Yikes. You should work on that. Next!", // Q2
+				"I would be insecure about that too... anyways. Next question.", // Q3
+				"Very revealing. Let's move on",
+				"Good luck with that.",
+				"You are gonna go far kid.",
+				"Ugh, me too girl, me too.",
+				"At least you have your priorities straight.",
+				"Sounds dreamy. We should go sometime.",
+				"Almost there.",
+				"AWKWARD.",
+				" I mean.. I guess that sounds reasonable?",
+				"SPICY! I think I know exactly what your sign is.", // Q3
+				// continue with addt'l question replies
+		};
 		return questionRepliesArray[n];
 	}
-
+//dec 5 kenneth duran
 	public void question3Choice13() {
 		System.out.println("Did I ask you to tell me your dream job wasn't on this list?");
 		String yesOrNo = input.next();
